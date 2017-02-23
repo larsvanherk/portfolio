d677bd177a6fa1e668bb3b1c5a702dcbf712f0a0
Lars van Herk <lars.v.herk@gmail.com>
2017-02-23 21:09:40 +0100

    WIP: configuring k8s /healthz endpoint.

--- index.js
+++ index.js
@@ -1,11 +1,19 @@
 var express = require('express');
+var force = require('forcedomain');
 var app = express();
+var healthz = express();
 var pjson = require('./package.json');
 
 var prefix = "[PORTFOLIO v" + pjson.version + "] ";
 
 // Disable server signature
 app.set('x-powered-by', false);
+
+// Force HTTPS & www-based domain
+app.use(force({
+    hostname: 'www.larsvanherk.com',
+    protocol: 'https'
+}));
 
 // Log all requests
 app.use(function(req, res, next) {
@@ -22,6 +30,17 @@ app.get('/', function (req, res) {
     res.sendFile('dist/index.html', { root : __dirname });
 });
 
+// Configure K8S health probe point
+
+healthz.set('x-powered-by', false);
+healthz.get('/healthz', function (req, res) {
+    res.sendStatus(200);
+});
+
 app.listen(5000, function() {
     console.log(prefix + "Server started!");
+
+    healthz.listen(5001, function() {
+        console.log(prefix + "Kubernetes health probe point started!");
+    });
 });
