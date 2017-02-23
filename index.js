0b2f81286d3d7d9908d976b37950d2d0776d609b
Lars van Herk <lars.v.herk@gmail.com>
2017-02-23 19:16:25 +0100

    Updates to deployment, disabled forceDomain redirect because kubernetes doesn't like a 301 on /.

--- index.js
+++ index.js
@@ -1,17 +1,11 @@
 var express = require('express');
 var app = express();
-var forceDomain = require('forcedomain');
 var pjson = require('./package.json');
 
 var prefix = "[PORTFOLIO v" + pjson.version + "] ";
 
 // Disable server signature
 app.set('x-powered-by', false);
-
-// Resolve non-www domain to www
-app.use(forceDomain({
-  hostname: 'www.larsvanherk.com'
-}));
 
 // Serve from dist folder
 app.use(express.static('dist'));
