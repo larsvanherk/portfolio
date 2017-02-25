905db7173061d8417a778faa8809681de2e36691
Lars van Herk <lars.v.herk@gmail.com>
2017-02-25 18:13:22 +0100

    Working on auto tag release in gulp.

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,12 @@
 var gulp         = require('gulp');
+var argv         = require('yargs').argv;
+var jeditor      = require("gulp-json-editor");
 var execSync     = require('child_process').execSync;
 var del          = require('del');
 var browserSync  = require('browser-sync');
 var autoprefixer = require('autoprefixer');
 var cssnano      = require('cssnano');
+var git          = require('gulp-git');
 var concat       = require('gulp-concat');
 var sass         = require('gulp-sass');
 var postcss      = require('gulp-postcss');
@@ -12,6 +15,8 @@ var imagemin     = require('gulp-imagemin');
 var htmlmin      = require('gulp-htmlmin');
 
 var version = require('./package.json').version;
+
+var releaseType = argv.release;
 
 gulp.task('default', ['build']);
 
@@ -70,15 +75,53 @@ gulp.task('serve', ['build'], function() {
   gulp.watch("src/index.html", ['html-watch']);
 });
 
-gulp.task('dist', ['build'], function() {
-  console.log('Building PORTFOLIO version ' + version + '!');
-  console.log('Waiting 10s before beginning...');
+gulp.task('release-version', function() {
+  return gulp.src('./package.json')
+        .pipe(jeditor(function (json) {
+          if(!['major', 'minor', 'patch'].contains(releaseType)) {
+            throw Error('Release version type not recognized: ')
+          }
 
-  setTimeout(function() {
-    execShell('docker build -t registry.gitlab.com/work-portfolio/portfolio:' + version + ' .');
-    execShell('docker push registry.gitlab.com/work-portfolio/portfolio:' + version);
-  }, 10000);
+          var version = json.version;
+
+          switch(releaseType) {
+            case 'major':
+              var ver_parts = version.split('.');
+              ver_parts[0] = parseInt(ver_parts[0]) + 1;
+
+              version = ver_parts.join('.');
+              break;
+
+            case 'minor':
+              var ver_parts = version.split('.');
+              ver_parts[1] = parseInt(ver_parts[1]) + 1;
+
+              version = ver_parts.join('.');
+              break;
+
+            case 'patch':
+              var ver_parts = version.split('.');
+              ver_parts[2] = parseInt(ver_parts[2]) + 1;
+
+              version = ver_parts.join('.');
+              break;
+          }
+
+          json.version = version;
+
+          return json;
+        }))
+        .pipe(gulp.dest('./package.json'));
   
+});
+
+gulp.task('release', ['release-version'], function() {
+  return gulp.src('./')
+        .pipe(git.checkout('master'))
+        .pipe(git.add())
+        .pipe(git.commit('PORTFOLIO release commit for version v' + version))
+        .pipe(git.tag(version, 'Release v' + version + ' for PORTFOLIO.'))
+        .pipe(git.push('origin', 'master', {args: ' --tags'}));
 });
 
 gulp.task('clean', function() {
